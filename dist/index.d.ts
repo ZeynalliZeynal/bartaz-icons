ea28d98764d1b38c98772bc7b2ea31bdca650ff0
Zeynalli Zeynal <zzeynalli446@gmail.com>
2025-01-28 22:56:24 +0400

    feat: add tsup to bundle ts

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+
+declare const SvgSafeBox2: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+
+export { SvgSafeBox2 as SafeBox2 };
